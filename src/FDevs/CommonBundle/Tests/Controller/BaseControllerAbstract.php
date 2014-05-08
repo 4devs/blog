e5420c11faff7b2bddefee5f1a201f0b12bcd2d2
Andrey Samusev <a.samusev@owox.com>
2014-05-08 14:13:00 +0400

    fix test

---
+++ src/FDevs/CommonBundle/Tests/Controller/BaseControllerAbstract.php
@@ -0,0 +1,51 @@
+<?php
+
+namespace FDevs\CommonBundle\Tests\Controller;
+
+use Symfony\Bundle\FrameworkBundle\Test\WebTestCase;
+use Symfony\Component\Routing\Router;
+
+abstract class BaseControllerAbstract extends WebTestCase
+{
+
+    /**
+     * init
+     */
+    public function __construct()
+    {
+        $this->getClient();
+    }
+
+    /**
+     * get Client
+     *
+     * @return \Symfony\Bundle\FrameworkBundle\Client
+     */
+    public function getClient()
+    {
+        return static::createClient();
+    }
+
+    /**
+     * get Container
+     *
+     * @return \Symfony\Component\DependencyInjection\ContainerInterface
+     */
+    public function getContainer()
+    {
+        return static::$kernel->getContainer();
+    }
+
+    /**
+     * generate Path
+     *
+     * @param string $name
+     * @param array $parameters
+     * @param bool $referenceType
+     * @return string
+     */
+    public function generatePath($name, $parameters = array(), $referenceType = Router::ABSOLUTE_PATH)
+    {
+        return $this->getContainer()->get('router')->generate($name, $parameters, $referenceType);
+    }
+}
