9e8711b6bc2d51baee8482b4a0df7ccc18462d71
andrey1s <andrey@4devs.org>
2014-12-18 18:47:40 +0300

    update packagist

--- web/app.php
+++ web/app.php
@@ -3,7 +3,7 @@
 use Symfony\Component\ClassLoader\ApcClassLoader;
 use Symfony\Component\HttpFoundation\Request;
 
-$loader = require_once __DIR__.'/../app/bootstrap.php.cache';
+$loader = require_once __DIR__.'/../var/bootstrap.php.cache';
 
 $loader = new ApcClassLoader('4blog', $loader);
 $loader->register(true);
